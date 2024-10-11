15a945cfc13f3b69b4452a65d312311c50773992
FarhanAttique <Farhanatique44@gmailcom>
2024-10-11 19:02:09 +0000

    first commit

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2024, Hakim and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+    refresh(frm) {
+        // Calculate total amount whenever the form is refreshed
+        calculate_total_amount(frm);
+    },
+    
+    flight_price: function(frm) {
+        // Recalculate total amount whenever flight price changes
+        calculate_total_amount(frm);
+    },
 
-// 	},
-// });
+    add_ons_add: function(frm) {
+        // Recalculate total amount whenever a new add-on is added
+        calculate_total_amount(frm);
+    },
+
+    add_ons_remove: function(frm) {
+        // Recalculate total amount whenever an add-on is removed
+        calculate_total_amount(frm);
+    }
+});
+
+function calculate_total_amount(frm) {
+    let total_addons = 0;
+
+    // Sum all add-on amounts
+    frm.doc.add_ons.forEach(function(row) {
+        total_addons += row.amount || 0;  // Ensure to handle undefined values
+    });
+
+    // Calculate total amount
+    frm.set_value('total_amount', frm.doc.flight_price + total_addons);
+}
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,19 @@
-# Copyright (c) 2024, Hakim and contributors
-# For license information, please see license.txt
-
-# import frappe
+import random
+import string
+import frappe
 from frappe.model.document import Document
 
-
 class AirplaneTicket(Document):
-	pass
+
+    def before_insert(self):
+        # Automatically assign a random seat in the format <random-integer><random-alphabet>
+        self.assign_random_seat()
+
+    def assign_random_seat(self):
+        # Generate a random integer between 1 and 99
+        seat_number = random.randint(1, 99)
+        # Randomly select a capital letter from A to E
+        seat_letter = random.choice(string.ascii_uppercase[:5])  # 'A' to 'E'
+        # Set the Seat field to the generated seat value
+        self.seat = f"{seat_number}{seat_letter}"
+
